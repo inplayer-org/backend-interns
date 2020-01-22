efc888489337eb3d7fe57afc00ab0fc10b7944ba
Filip <Filip.Krstevski@inplayer.com>
2020-01-22 10:35:43 +0100

    account structure

--- BankAccount/pkg/entities/account.go
+++ BankAccount/pkg/entities/account.go
@@ -1,11 +1,13 @@
 package entities
 
+import "time"
+
 type Account struct {
 	Id        int     `db:"id"`
 	UserId    int  	  `db:"user_id"`
 	Balance   float64 `db:"balance"`
 	Currency  string  `db:"currency"`
 	Status    bool    `db:"status"`
-	CreatedAt string  `db:"created_at"`
-	UpdatedAt string  `db:"updated_at"`
+	CreatedAt time.Time  `db:"created_at"`
+	UpdatedAt time.Time  `db:"updated_at"`
 }
