278a4d6c707456add1450a8e6b9d599879d539ae
Filip <Filip.Krstevski@inplayer.com>
2020-01-30 09:39:36 +0100

    account store test (testing merge)

---
+++ BankAccount/pkg/tests/accountstore_test.go
@@ -0,0 +1,57 @@
+
+
+func (suite *AccountTestSuite) TestUpdateAccount() {
+	store := store.NewAccountStoreModel(suite.Db)
+	var err error
+	var account *entities.Account
+	var accounts []*entities.Account
+	//time.Sleep(2 * time.Second)
+	for _, value := range suite.AccountsP {
+		account, err = store.UpdateAccount(value.Id, value.UserId, value.Balance, value.Currency)
+		if err != nil {
+			suite.T().Fatal("Unable to run UpdateAccount store func")
+		}
+		accounts = append(accounts, account)
+	}
+	for _, value := range suite.AccountsP {
+		for i := range accounts {
+			if value.Id == accounts[i].Id {
+				suite.Equal(value.UserId, accounts[i].UserId)
+				suite.Equal(value.Balance, accounts[i].Balance)
+				suite.Equal(value.Currency, accounts[i].Currency)
+			}
+		}
+	}
+}
+func (suite *AccountTestSuite) TestCloseAccount() {
+	store := store.NewAccountStoreModel(suite.Db)
+	var err error
+	var account *entities.Account
+	var accounts []*entities.Account
+	for _, value := range suite.AccountsP {
+		account, err = store.CloseAccount(value.Id, value.UserId)
+		if err != nil {
+			suite.T().Fatal("Unable to run CloseAccount store func")
+		}
+		accounts = append(accounts, account)
+	}
+	for _, value := range suite.AccountsP {
+		for i := range accounts {
+			if value.Id == accounts[i].Id {
+				suite.Equal(value.UserId, accounts[i].UserId)
+				suite.Equal(false, accounts[i].Status)
+			}
+		}
+	}
+}
+func TestAccountTestSuite(t *testing.T) {
+	suite.Run(t, new(AccountTestSuite))
+}
+func (suite *AccountTestSuite) TearDownTest() {
+	for i := 0; i < len(suite.AccountsP); i++ {
+		_, err := suite.Db.Exec("DELETE FROM Account WHERE id=?", suite.AccountsP[i].Id)
+		if err != nil {
+			suite.T().Fatal("Unable to run delete query")
+		}
+	}
+}
